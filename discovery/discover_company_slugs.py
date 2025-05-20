ca00e2aa124b548aa9e2d104e04aab25c7fd38e8
chapmanhk <hannahxchapman@yahoo.com>
2025-05-20 15:01:07 -0700

    Add logging for script

--- discovery/discover_company_slugs.py
+++ discovery/discover_company_slugs.py
@@ -4,6 +4,17 @@ from urllib.parse import urlparse # Breaks URLs into components like domain and 
 import re
 import os # Used to check for environment variables and file existence
 import json # used to save and load the slug list as a .json file
+import logging # Create logs for auditing
+
+# Basic log configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler(), # Log to console
+        logging.FileHandler("logs/discovery.log") # Log to file
+    ]
+)
 
 # Identifies the source and extracts the first part of the path (company slug)
 def extract_company_slug(url):
@@ -52,16 +63,27 @@ def save_slugs(slugs, filepath="company_slugs.json"):
         existing = {"lever": [], "greenhouse": []} # If not, initialize with empty lists for each source
 
     for source in ["lever", "greenhouse"]: # Loop through both Lever and Greenhouse to update their slug lists
-        updated = set(existing.get(source, [])) | slugs[source] # Merge new slugs with existing ones using a set union (removes duplicates)
+        existing_set = set(existing.get(source, [])) # Looks up existing list of slugs as a set
+        new_slugs = slugs[source]
+
+        # Find which slugs are new
+        new = new_slugs - existing_set
+        if new:
+            logging.info(f"New {source.title()} companies discovered: {', '.join(sorted(new))}")
+        else:
+            logging.info(f"No new {source.title()} companies discovered.")
+        updated = existing_set | new_slugs # Merge new slugs with existing ones using a set union (removes duplicates)
         existing[source] = sorted(updated) # Save the sorted list of updated slugs
 
     with open(filepath, "w") as f:
         json.dump(existing, f, indent=2) # Write the updated slug dictionary back to the file
-    print(f"Saved {len(existing['lever'])} Lever slugs and {len(existing['greenhouse'])} Greenhouse slugs to {filepath}")
+        logging.info(f"Saved {len(existing['lever'])} Lever slugs and {len(existing['greenhouse'])} Greenhouse slugs to {filepath}")
+
 
 if __name__ == "__main__": # Entry point
     API_KEY = os.environ.get("SERPAPI_KEY") # Get the serpAPI key from the environment variables
     if not API_KEY: 
+        logging.error("Missing SERPAPI_KEY. Exiting.")
         raise Exception("Please set your SERPAPI_KEY in environment variables.") # Raise an error if the API key is missing
     
     query = 'site:jobs.lever.co OR site:boards.greenhouse.io' # Define the search query for Lever and Greenhouse job posts
